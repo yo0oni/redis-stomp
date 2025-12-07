e6de6dbfdb90bb0eac81ea754913297d6e82ada3
rlawldbs0911 <rlawldbs0911@naver.com>
2025-12-07 16:07:22 +0900

    refactor: @SendTo 대신 SimpMessageSendingOperations로 메시지 전송 방식 변경

--- chatserver/src/main/java/com/example/chatserver/chat/controller/StompController.java
+++ chatserver/src/main/java/com/example/chatserver/chat/controller/StompController.java
@@ -3,16 +3,30 @@ package com.example.chatserver.chat.controller;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
+import com.example.chatserver.chat.dto.ChatMessageReqDto;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class StompController {
 
-	@MessageMapping("/{roomId}") // 클라이언트에서 특정 /publish/roodId 형태로 메시지를 발행 시 MessageMapping이 수신
-	@SendTo("/topic/{roomId}") // 해당 rootId에 메시지를 발행하여 구독중인 클라이언트에게 메시지 전송
-	// DestinationVariable 은 @MessageMapping 어노테이션으로 정의한 Websocket Controller 내에서만 사용
-	public String sendMessage(@DestinationVariable Long roomId, String message) {
-		System.out.println(message);
-		return message;
+	private final SimpMessageSendingOperations messageTemplate;
+
+	// @MessageMapping("/{roomId}") // 클라이언트에서 특정 /publish/roodId 형태로 메시지를 발행 시 MessageMapping이 수신
+	// @SendTo("/topic/{roomId}") // 해당 rootId에 메시지를 발행하여 구독중인 클라이언트에게 메시지 전송
+	// // DestinationVariable 은 @MessageMapping 어노테이션으로 정의한 Websocket Controller 내에서만 사용
+	// public String sendMessage(@DestinationVariable Long roomId, String message) {
+	// 	System.out.println(message);
+	// 	return message;
+	// }
+
+	@MessageMapping("/{roomId}")
+	public void sendMessage(@DestinationVariable Long roomId, ChatMessageReqDto chatMessageReqDto) {
+		System.out.println(chatMessageReqDto.getMessage());
+		messageTemplate.convertAndSend("/topic/"+roomId, chatMessageReqDto);
 	}
 }
